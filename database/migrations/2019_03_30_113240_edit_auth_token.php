c7a4828152dd305eb94d55207ecb98e0ed367d6d
hafidd <hafid.f.septiawan@gmail.com>
2019-03-30 11:33:54 +0700

    edit migration

--- database/migrations/2019_03_30_113240_edit_auth_token.php
+++ database/migrations/2019_03_30_113240_edit_auth_token.php
@@ -1,10 +1,10 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
-class AddAuthToken extends Migration
+class EditAuthToken extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddAuthToken extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('auth_token')->nullable()->after('email');
+            $table->string('auth_token')->nullable()->change();
         });
     }
 
@@ -26,7 +26,7 @@ class AddAuthToken extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('auth_token');
+            //
         });
     }
 }
